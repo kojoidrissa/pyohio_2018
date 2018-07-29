c34ea6f54b777f36895f96ad3fa12713587c32e9
Kojo Idrissa <kojo.idrissa@gmail.com>
2018-07-29 17:06:10 -0400

    added Lightning Talk slides

--- kojo_function.py
+++
@@ -1,8 +0,0 @@
-def kojo_at_cmh(name):
-    """
-    str -> str
-    Takes a string and returns a string customized with the input string
-    """
-
-    # I wanted to use an f-string here, but I forgot how. But they're faster than the str.format method
-    print("Hi {}, Kojo is at PyOhio 2018".format(name))
